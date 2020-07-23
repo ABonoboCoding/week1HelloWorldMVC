fceb4aa79741a535c6cd10cf025fbeb85703383a
Minxuan Lin <James.lin1314520@outlook.com>
2020-07-23 23:35:24 +0930

    Added a new Controller. But followed instruction to modify original controller. Original controller modified to produce correct output when changing URL inputs.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,37 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using MvcMovie.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
-    public class HomeController : Controller
+    public class HelloWorldController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        // 
+        // GET: /HelloWorld/
 
-        public HomeController(ILogger<HomeController> logger)
+        public string Index()
         {
-            _logger = logger;
+            return "This is my default action...";
         }
 
-        public IActionResult Index()
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int ID = 1)
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
